6886476efb43c92758065a92e7b6cfd6d4a20925
HydorgenBomb <linguabano@gmail.com>
2025-10-24 17:36:37 -0600

    se subio proyecto

---
+++ Avance1/src/main/java/sodxpunk/avance1/Avance1.java
@@ -0,0 +1,17 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sodxpunk.avance1;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class Avance1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Avance1/src/main/java/sodxpunk/avance1/Fecha.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sodxpunk.avance1;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class Fecha extends GregorianCalendar{
+
+    public Fecha() {
+    }
+    public Fecha (int dia, int mes, int anio){
+        
+    }
+    public Fecha (fecha Fecha){
+        
+    }
+    public Fecha (String Fecha){
+        
+    }
+}
