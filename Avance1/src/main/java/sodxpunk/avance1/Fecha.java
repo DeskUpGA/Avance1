dda670911f125d43596b846e97437e6d013c27ff
HydorgenBomb <linguabano@gmail.com>
2025-10-25 19:50:50 -0700

    movimiento de la clase fecha

--- Avance1/src/main/java/sodxpunk/avance1/Fecha.java
+++ Avance1/src/main/java/sodxpunk/avance1/Fecha.java
@@ -12,13 +12,28 @@ public class Fecha extends GregorianCalendar{
 
     public Fecha() {
     }
-    public Fecha (int dia, int mes, int anio){
-        
+
+    public Fecha(int year, int month, int dayOfMonth) {
+        super(year, month, dayOfMonth);
     }
+    
     public Fecha (fecha Fecha){
         
     }
     public Fecha (String Fecha){
         
     }
+
+    public static int getYEAR() {
+        return YEAR;
+    }
+
+    public static int getMONTH() {
+        return MONTH;
+    }
+
+    public static int getDATE() {
+        return DATE;
+    }
+    
 }
