d83fcc8a80694dd910f1f11cd53af3adba98d0eb
HydorgenBomb <linguabano@gmail.com>
2025-11-07 17:22:56 -0600

    el new main

---
+++ Avance1/src/main/java/sodxpunk/avance1/Ejemplo.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sodxpunk.avance1;
+import java.util.Scanner;
+/**
+ *
+ * @author Laboratorios
+ */
+public class Ejemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Fecha f1 = new Fecha();
+        System.out.println("La fecha de hoy es: " + f1);
+        System.out.println("Escriba una nueva fecha en el formato DD/MM/YYYY");
+        String f2 = sc.nextLine();
+        Fecha F2 = new Fecha(f2);
+        int dias = F2.lapso(f1);
+        Periodo p0 = new Periodo(f1,F2);
+        System.out.println("El primer periodo es: " + p0);
+        System.out.println("El lapso contiene: " + dias + " dia/dias");
+        F2 = F2.vencimiento(7);
+        System.out.println("A su fecha se le agregaron 7 dias");
+        Periodo p1 = new Periodo(f1,F2);
+        System.out.println("Su nuevo periodo es: " + p1);
+        System.out.println("¿que parte de la fecha va a modificar?");
+        System.out.println("1. dia");
+        System.out.println("2. mes");
+        System.out.println("3. año");
+        System.out.println("4. no modificar");
+        System.out.println("Seleccione la opccion (1-4)");
+        int opcion = sc.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.print("Ingrese el nuevo día: ");
+                int nuevoDia = sc.nextInt();
+                F2.setDia(nuevoDia);
+                break;
+            case 2:
+                System.out.print("Ingrese el nuevo mes: ");
+                int nuevoMes = sc.nextInt();
+                F2.setMes(nuevoMes);
+                break;
+            case 3:
+                System.out.print("Ingrese el nuevo año: ");
+                int nuevoAnio = sc.nextInt();
+                F2.setAño(nuevoAnio);
+                break;
+            case 4:
+                System.out.println("No se modificará la fecha.");
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                break;
+        }
+        System.out.println("Su fecha la ha modificado a: " + F2);
+        int dias = F2.lapso(F2);
+        System.out.println("Se encuentra la nueva fecha: ");
+    }   
+}
